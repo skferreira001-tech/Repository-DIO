e6a227df16315835d23bb23dd96c80554d4d76f9
skferreira <skferreira001@gmail.com>
2025-12-29 18:09:53 -0300

    Inclusão Atividade Prática

---
+++ Checkpoints/Introdução_ao_JAVASCRIPT/atividadePratica/index.js
@@ -0,0 +1,37 @@
+const alunos = [
+    {
+        nome: 'Mario',
+        nota: 6,
+        turma: '1A',
+    },
+    {
+        nome: 'Luana',
+        nota: 7,
+        turma: '1A',
+    },
+    {
+        nome: 'Luiz',
+        nota: 5,
+        turma: '1A',
+    },
+    {
+        nome: 'Magda',
+        nota: 8,
+        turma: '1A',
+    },
+];
+
+function alunosAprovados (arr, media) {
+    let aprovados = [];
+   
+    for(i=0; i<arr.length; i++); {
+        const {nota, nome} = arr[i];
+
+        if(nota >= media) {
+            aprovados.push(nome);
+        }
+    }
+    
+    return aprovados;
+}
+console.log(alunosAprovados(alunos, 5))
